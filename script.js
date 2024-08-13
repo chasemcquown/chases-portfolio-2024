94150f21f3563a85f8e6f70122ff09c90281ccc9
chasem <cpmcquown@gmail.com>
2024-08-13 16:21:22 -0700

    adds

---
+++ script.js
@@ -0,0 +1,8 @@
+// MOBILE MENU LOGIC
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+  
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
